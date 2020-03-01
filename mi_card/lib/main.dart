237ed770e42b5a2cde40b9d8e6a82ef287fbdf7d
Dilmurod777 <valiev.dilmurod1998@gmail.com>
2020-03-01 13:07:48 +0500

    add mi_card app

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -1,10 +1,76 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.teal,
-          body: Container(),
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 80.0,
+                backgroundImage: AssetImage('assets/images/profile.jpg'),
+              ),
+              Text(
+                'Dilmurod Valiev',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.teal.shade100,
+                  fontSize: 18.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+                width: 150.0,
+                child: Divider(color: Colors.teal.shade100),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.teal),
+                  title: Text(
+                    '+998(91) 539-79-89',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: "SourceSansPro",
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.mail, color: Colors.teal),
+                  title: Text(
+                    'valiev.dilmurod1998@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: "SourceSansPro",
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+}
